e19bfdc3587d33552c4e7aab68d4e137d42fd8d1
Leon Malisov <leonmalisov@gmail.com>
2020-01-06 19:40:43 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,24 @@
+def display_board(board)
+  puts (" #{board[0]} | #{board[1]} | #{board[2]} ")
+  puts ("-----------")
+  puts (" #{board[3]} | #{board[4]} | #{board[5]} ")
+  puts ("-----------")
+  puts (" #{board[6]} | #{board[7]} | #{board[8]} ")
+  puts
+end
+
+def input_to_index(user_input)
+  user_input = user_input.to_i
+  index = user_input - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else false
+  end
+end
+
+def move(array, index, player = "X")
+  array[index] = player
+end
